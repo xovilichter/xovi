7dd025f94e67112b9b44b4a072e9b2a12ebb5147
root <root@vps67145.ovh.net>
2014-05-21 18:54:02 +0200

    Automatic Commit

--- xovilichter_xovi_scraper.php
+++ xovilichter_xovi_scraper.php
@@ -1,32 +1,44 @@
 <?php 
 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, "http://www.xovi.de/xovilichter/"); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_USERAGENT, 'Xovilichter.Dynapso/1.0 (+https://github.com/xovilichter/xovi)');
-curl_setopt($ch, CURLOPT_REFERER, 'https://github.com/xovilichter/xovi'); 
-$output = curl_exec($ch);
-curl_close($ch);
+// set default time zone
+date_default_timezone_set('Europe/Berlin');
 
-// den code ein wenig aufräumen
-$output = str_replace("\n", '', $output);
-$output = str_replace("\t", '', $output);
-$output = str_replace('itemprop="itemListElement"', ' ', $output);
-$output = str_replace('  ', ' ', $output);
-$output = str_replace('  ', ' ', $output);
-$output = str_replace('  ', ' ', $output);
-$output = str_replace('  ', ' ', $output);
-$output = str_replace(' <', '<', $output);
-
-preg_match_all('#<div class="line"><span class="(.*)">(.*).</span><span class="(.*)"><a href="(.*)" target="_blank">(.*)</a></span><span class="(.*)">(.*)</span><span class="(.*)">(.*)</span><span class="gplus">(.*)</span></div>#Uis', $output, $matches);
-
-$search_results = array();
-
-foreach($matches[2] as $n => $position){
-	$search_results[] = array(
-		'position' => $position,
-		'url' => $matches[4][$n],
+function xovilichter_xovi_scraper(){
+	
+	// curl
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, "http://www.xovi.de/xovilichter/"); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Xovilichter.Dynapso/1.0 (+https://github.com/xovilichter/xovi)');
+	curl_setopt($ch, CURLOPT_REFERER, 'https://github.com/xovilichter/xovi'); 
+	$output = curl_exec($ch);
+	curl_close($ch);
+	
+	// code cleanup
+	$output = str_replace("\n", '', $output);
+	$output = str_replace("\t", '', $output);
+	$output = str_replace('itemprop="itemListElement"', ' ', $output);
+	$output = str_replace('  ', ' ', $output);
+	$output = str_replace('  ', ' ', $output);
+	$output = str_replace('  ', ' ', $output);
+	$output = str_replace('  ', ' ', $output);
+	$output = str_replace(' <', '<', $output);
+	
+	// match results
+	preg_match_all('#<div class="line"><span class="(.*)">(.*).</span><span class="(.*)"><a href="(.*)" target="_blank">(.*)</a></span><span class="(.*)">(.*)</span><span class="(.*)">(.*)</span><span class="gplus">(.*)</span></div>#Uis', $output, $matches);
+	
+	$search_results = array();
+	
+	foreach($matches[2] as $n => $position){
+		$search_results[] = array(
+			'position' => $position,
+			'url' => $matches[4][$n],
+		);
+	}
+	
+	return array(
+		'last_update' => date('Y-m-d H:i'),
+		'search_results' => $search_results,
 	);
+	
 }
-
-print_r($search_results);
